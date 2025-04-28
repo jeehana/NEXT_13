b0d83e742a53e4be35ec0abc6a31f8533a752cf8
jeehana <ajhan5552@gmail.com>
2025-04-28 20:26:26 +0900

    fix

--- shopping-cart/src/context/CartContext.js
+++ shopping-cart/src/context/CartContext.js
@@ -5,7 +5,16 @@ export const CartContext = createContext();
 
 // 2. Provider 컴포넌트 정의
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(() => {
+        // 로컬 스토리지에서 장바구니 데이터를 가져옴
+        const savedCart = localStorage.getItem("cart");
+        return savedCart ? JSON.parse(savedCart) : [];
+    });
+
+    useEffect(() => {
+        // 장바구니 데이터가 변경될 때마다 로컬 스토리지에 저장
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }, [cart]);
 
     return (
         <CartContext.Provider value={{ cart, setCart }}>
